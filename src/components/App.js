44b66d27d19ab9c761c678669229af4521a0dbb4
YichengShen <jsszsyc@hotmail.com>
2021-04-19 19:42:12 -0400

    Change from BrowserRouter to HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Grid } from "@material-ui/core";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
