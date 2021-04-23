d0084ee296adfbfaec05bbe7ea3b5bfb90b3dccb
YichengShen <jsszsyc@hotmail.com>
2021-04-23 17:45:16 -0400

    fix

--- src/components/AvatarBadges.js
+++ src/components/AvatarBadges.js
@@ -1,110 +1,29 @@
 import React from "react";
-import { Box, Button, Grid, Paper, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { green, red } from "@material-ui/core/colors";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import theme from "../theme";
 
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        maxWidth: '36ch',
-        backgroundColor: theme.palette.background.paper,
-    },
-    inline: {
-        display: 'inline',
-    },
-}));
+const BadgesTest = (props) => {
+  const { BadgeName, BadgeImageUrl, BadgeExplanations, BadgeStatus } = props;
 
-
-const surveyTheme = createMuiTheme({
-    palette: {
-        secondary: { main: theme.palette.secondary.main },
-    },
-    overrides: {
-        MuiToggleButtonGroup: {
-            root: {
-                padding: "2px 5px",
-                borderRadius: "15px",
-                backgroundImage: `linear-gradient(${green[500]}, ${red[300]})`,
-            },
-        },
-        MuiToggleButton: {
-            sizeLarge: {
-                padding: "15px 45px",
-            },
-            root: {
-                color: "white",
-                backgroundColor: "rgba(0, 0, 0, .5)",
-                fontWeight: "lighter",
-                margin: "3px 0",
-                borderRadius: 10,
-                "&:hover": {
-                    color: "black",
-                    backgroundColor: "rgba(255, 255, 255, .4)",
-                },
-                "&$selected": {
-                    color: "black",
-                    backgroundColor: "rgba(255, 255, 255, .8)",
-                    fontWeight: "bolder",
-                    "&:hover": {
-                        backgroundColor: "rgba(255, 255, 255, .4)",
-                    },
-                },
-            },
-        },
-        MuiButton: {
-            root: {
-                backgroundColor: "rgba(255, 255, 255, .2)",
-            },
-        },
-    },
-});
-
-
-
-
-const BadgesTest = (props, theme) => {
-
-    const classes = useStyles();
-    const {
-        BadgeName,
-        BadgeImageUrl,
-        BadgeExplanations,
-        BadgeStatus,
-    } = props;
-
-
-
-    return (
-        <MuiThemeProvider theme={surveyTheme}>
-            <Grid container>
-                <Grid item xs={1} sm={2} md={3} />
-                <Grid item xs={10} sm={8} md={6}>
-                    <List className={classes.root}>
-                        <ListItem alignItems="flex-start">
-                            <ListItemAvatar>
-                                <Avatar alt="BadgeImage" src= {BadgeImageUrl} />
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary={BadgeExplanations}
-                            />
-                        </ListItem>
-                        <Divider variant="inset" component="li" />
-
-                    </List>
-                </Grid>
-                <Grid item xs={1} sm={2} md={3} />
-            </Grid>
-        </MuiThemeProvider>
-    );
+  return (
+    <>
+      <ListItem key={BadgeExplanations} alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="BadgeImage" src={BadgeImageUrl} />
+        </ListItemAvatar>
+        <ListItemText primary={BadgeExplanations} />
+      </ListItem>
+      <Divider variant="inset" />
+    </>
+  );
 };
 
 export default BadgesTest;
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,17 +1,29 @@
-import React,{ useState } from "react";
-import firebase from "../services/firebase"
+import React, { useState } from "react";
+import firebase from "../services/firebase";
 import { firestore as db } from "../services/firebase";
 import startOfToday from "../utils/startOfToday";
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import AvatarBadges from "../components/AvatarBadges";
+import { Paper, Grid, Typography } from "@material-ui/core";
+import List from "@material-ui/core/List";
+
+import { makeStyles } from "@material-ui/core/styles";
 import BadgesTest from "../components/AvatarBadges";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    backgroundColor: theme.palette.primary.light,
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(10),
+    padding: "30px 0px",
+    display: "flex",
+    flexDirection: "column",
+  },
+  greenBold: {
+    color: theme.palette.secondary.light,
+    fontWeight: "bolder",
+    marginBottom: "20px",
+  },
+}));
+
 function twoDimensionArray(a, b) {
   let arr = [];
   for (let i = 0; i < a; i++) {
@@ -24,17 +36,15 @@ function twoDimensionArray(a, b) {
 function parse2DArray(array1, index) {
   var arrayOfItem = [];
   var i;
-  if (typeof (array1[0][index]) == "number") {
+  if (typeof array1[0][index] == "number") {
     for (i = 0; i < array1.length; i++) {
       arrayOfItem.push(parseInt(array1[i][index]));
     }
-  }
-  else if (typeof (array1[0][index]) == "string") {
+  } else if (typeof array1[0][index] == "string") {
     for (i = 0; i < array1.length; i++) {
       arrayOfItem.push(String(array1[i][index]));
     }
-  }
-  else if (typeof (array1[0][index]) == "object") {
+  } else if (typeof array1[0][index] == "object") {
     for (i = 0; i < array1.length; i++) {
       arrayOfItem.push(Object.values(array1[i][index])[0]);
     }
@@ -43,27 +53,31 @@ function parse2DArray(array1, index) {
 }
 let publicurl = process.env.PUBLIC_URL;
 let badgesArray = twoDimensionArray(4, 0);
-let BadgeName = ["3daysConsective",
+let BadgeName = [
+  "3daysConsective",
   "7daysConsecutive",
   "TotalMoreThan5days",
   "TodayTop",
-  "TodayAboveAverage"];
+  "TodayAboveAverage",
+];
 let BadgeImages = [
-  publicurl + "/assets/p4.png",//0
+  publicurl + "/assets/p4.png", //0
   publicurl + "/assets/p5.png",
   publicurl + "/assets/p3.png",
   publicurl + "/assets/p7.png",
   publicurl + "/assets/p6.png",
-  publicurl + "/assets/p4_1.png",//5
+  publicurl + "/assets/p4_1.png", //5
   publicurl + "/assets/p5_1.png",
   publicurl + "/assets/p3_1.png",
   publicurl + "/assets/p7_1.png",
-  publicurl + "/assets/p6_1.png",];
-let BadgeExplanations = ["You have checked in for 3 consecutive days! ",
+  publicurl + "/assets/p6_1.png",
+];
+let BadgeExplanations = [
+  "You have checked in for 3 consecutive days! ",
   "You have checked in for 7 consecutive days! ",
   "You have checked in for More than 5 days in total! ",
-  "You get the top score for you today's transportation choice! Go Green!",
-  "You performed above average! Thank you for your contribution to the environment"
+  "You got the top score for you today's transportation choice! Go Green!",
+  "You performed above average! Thank you for your contribution to the environment!",
 ];
 var x;
 for (x = 0; x < BadgeName.length; x++) {
@@ -73,34 +87,36 @@ for (x = 0; x < BadgeName.length; x++) {
   temp.push(BadgeExplanations[x]);
   temp.push(0);
   badgesArray.push(temp);
-}//initalize status
-
+} //initalize status
 
 const Badges = () => {
-  var scores = [];
-  var UserId = [];
-  var AllScores = [];
-  var AllUserId = [];
+  const classes = useStyles();
+
   var i;
+  var UserId = [];
+  const [scores, setScores] = useState([]);
+  const [userId, setUserId] = useState([]);
   const CurrentUserID = firebase.auth().currentUser.uid;
   var AllData = twoDimensionArray(3, 0);
   //getting data
   const getAllScore = async () => {
-    return db.collection("surveys")
+    return db
+      .collection("surveys")
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          var a = []
+          var a = [];
           a.push(parseInt(doc.data().score));
-          a.push(String(doc.data().uid))
-          a.push(doc.data().date)
+          a.push(String(doc.data().uid));
+          a.push(doc.data().date);
           AllData.push(a);
         });
         return AllData;
       });
-  }
+  };
   const getTodayScore = async () => {
-    return db.collection("surveys")
+    return db
+      .collection("surveys")
       .where("date", ">", startOfToday())
       .get()
       .then((querySnapshot) => {
@@ -109,9 +125,10 @@ const Badges = () => {
         });
         return scores;
       });
-  }
+  };
   const getTodayId = async () => {
-    return db.collection("surveys")
+    return db
+      .collection("surveys")
       .where("date", ">", startOfToday())
       .get()
       .then((querySnapshot) => {
@@ -120,16 +137,14 @@ const Badges = () => {
         });
         return UserId;
       });
-  }
+  };
   //functions to qualify badges' conditions
   function checkin(AllData) {
-
     let TempTimeArray = parse2DArray(AllData, 2);
     let IdArray = parse2DArray(AllData, 1);
     let OnedaySeconds = 24 * 60 * 60;
     //let IndexArray = [];
     var i, x, max, countConsecutive;
-    var countArray = [];
     var tempCountArray = [];
     var result = [];
     var TimeArray = [];
@@ -142,60 +157,52 @@ const Badges = () => {
     for (x = 0; x < TimeArray.length; x++) {
       if (Math.abs(TimeArray[x] - TimeArray[x + 1]) <= OnedaySeconds) {
         countConsecutive = countConsecutive + 1;
-      }
-      else if (x + 1 == TimeArray.length) {
+      } else if (x + 1 === TimeArray.length) {
         tempCountArray.push(countConsecutive);
-      }
-      else {
+      } else {
         tempCountArray.push(countConsecutive);
         countConsecutive = 1; //reset countConsecutive
       }
     }
     max = Math.max.apply(null, tempCountArray);
-    console.log("test", max);
     result.push(max, TimeArray.length);
     //first is consecutive login in days,
     // second is totol check in days.
-    console.log(result);
     return result;
   }
 
   function AboveAvg(scores) {
     var arrSum = 0;
-    var temp = 0
+    var temp = 0;
     for (i = 0; i < scores.length; i++) {
-      arrSum = temp + scores[i]
-    };
+      arrSum = temp + scores[i];
+    }
+    var avg = arrSum / scores.length;
     let index = UserId.indexOf(CurrentUserID);
     if (index == null) {
-      console.log("CurrentUser not found");
       return 0;
-    }
-    else {
-      let topScore = scores.sort(function (a, b) { return a - b });
-      if (scores[index] == topScore) {
-        return 2;//top Score 
-      }
-      else if (scores.index >= arrSum) {
-        return 1;//above average
-      }
-      else {
-        return -1;//below average
+    } else {
+      let topScore = Math.max(...scores);
+
+      if (scores[index] === topScore) {
+        return 2; //top Score
+      } else if (scores[index] >= avg && scores[index] !== topScore) {
+        return 1; //above average
+      } else {
+        return -1; //below average
       }
     }
-  };
+  }
   function checkconsecutive(consecutive, array1) {
     if (consecutive < 3) {
       badgesArray[0][3] = 0;
       badgesArray[0][1] = BadgeImages[5];
       badgesArray[1][1] = BadgeImages[6];
-    }
-    else if (consecutive < 7 && consecutive >= 3) {
+    } else if (consecutive < 7 && consecutive >= 3) {
       badgesArray[0][3] = 1;
       badgesArray[0][1] = BadgeImages[0];
       badgesArray[1][1] = BadgeImages[6];
-    }
-    else if (consecutive >= 7) {
+    } else if (consecutive >= 7) {
       badgesArray[0][3] = 1;
       badgesArray[1][3] = 1;
       badgesArray[0][1] = BadgeImages[5];
@@ -223,56 +230,72 @@ const Badges = () => {
         badgesArray[3][1] = BadgeImages[3];
         break;
     }
-    let temp = checkin(AllData)
+    let temp = checkin(AllData);
     let consecutive = temp[0];
     let total = temp[1];
     checkconsecutive(consecutive, badgesArray);
     if (total >= 5) {
       badgesArray[2][3] = 1;
-      badgesArray[2][1] = BadgeImages[7];
-    }
-    else {
       badgesArray[2][1] = BadgeImages[2];
+    } else {
+      badgesArray[2][1] = BadgeImages[7];
     }
     return badgesArray;
   }
-var statusArray;
-const [arr, setArr] = useState([]);
+  const [arr, setArr] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   React.useEffect(() => {
     async function check() {
       try {
+        setLoading(true);
         let c = await getTodayScore();
         let a = await getAllScore();
         let b = await getTodayId();
-        let scores = Object.values(c);
-        let UserId = Object.values(b);
-        let AllData = Object.values(a);//return scores, userid, t(time)
-         setArr(BadgeHandler());
-        //console.log(statusArray);
+
+        if (loading) {
+          setScores(Object.values(c));
+          setUserId(Object.values(b));
+          //let AllData = Object.values(a); //return scores, userid, t(time
+        }
+        setArr(BadgeHandler());
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     }
     check();
   }, []);
-  console.log(arr);
+
+  if (loading) {
+    return <div style={{ color: "white" }}>loading...</div>;
+  }
 
   return (
     <React.Fragment>
-      {arr.forEach(element => (
-        <BadgesTest
-          BadgeImageUrl={element[1]}
-          BadgeExplanations={element[2]}
-        />
-      )) }
+      <Grid container direction="row">
+        <Grid item xs={1} sm={2} md={3} />
+        <Grid item xs={10} sm={8} md={6}>
+          <Paper className={classes.paper} elevation={10}>
+            <List>
+              {arr.map((element, index) => {
+                return (
+                  <>
+                    <BadgesTest
+                      key={index}
+                      BadgeImageUrl={element[1]}
+                      BadgeExplanations={element[2]}
+                    />
+                  </>
+                );
+              })}
+            </List>
+          </Paper>
+        </Grid>
+        <Grid item xs={1} sm={2} md={3} />
+      </Grid>
     </React.Fragment>
-
-  )
+  );
 };
 
 export default Badges;
-
-
-
-
